f858a1f72ae7d738c559fec752f2b929ceb52f32
Andrii Kurinnyi <andrew@marpasoft.com>
2011-06-12 20:49:15 -0700

    Added urls for serving static files and media in the development mode

--- urls.py
+++ urls.py
@@ -13,3 +13,16 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG:
+    urlpatterns += patterns('', (
+        r'^site_media/media/(?P<path>.*)$',
+        'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT}
+        ),
+    )
