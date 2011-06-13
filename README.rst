895c94988afd51f0bac3cee0005f48571a651c68
Andrii Kurinnyi <andrew@marpasoft.com>
2011-06-12 20:33:17 -0700

    Initial commit

---
+++ README.rst
@@ -0,0 +1,13 @@
+Django Zero Project
+===================
+
+
+Template for django-startproject built according to my personal preferences
+
+::
+
+    pip install -e git://github.com/lincolnloop/django-startproject.git#egg=django-startproject
+
+    git clone git://github.com/zen4ever/django_zero_project
+
+    django-startproject.py --template-dir=django_zero_project yourprojectname
---
+++ urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls.defaults import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'zero_template.views.home', name='home'),
+    # url(r'^zero_template/', include('zero_template.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
